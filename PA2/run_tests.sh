25c66ffa523cd51f892d47fe4449fea970c54b45
Mathan Glezer <mathan.glezer@gmail.com>
2023-10-02 16:22:09 -0400

    Add automated testing target + script.

---
+++ PA2/run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+errors=false;
+for file in ./*test.cl; do
+    if [ -f ${file} ]; then
+	output=$(diff <(./lexer $file) <(lexer $file))
+	if [ -n "$output" ]; then
+	    echo "Diff in file ${file}:\n'${output}'";
+	    errors=true;
+	fi;
+    fi;
+done;
+if [ $errors = false ]; then
+  printf "\n\nAll tests pass!\n";
+fi
+
